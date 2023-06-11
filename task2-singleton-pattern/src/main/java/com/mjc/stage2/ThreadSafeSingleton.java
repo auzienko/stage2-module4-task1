28fe0d0846d45eaf3ceaf86bf3caa520dd27d907
Aleksandr Uzienko <a.uzienko@gmail.com>
2023-06-11 12:36:53 +0300

    solution

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -2,4 +2,22 @@ package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
     // Write your code here!
+    private static volatile ThreadSafeSingleton instatce;
+    private static final Object mutex = new Object();
+
+    private ThreadSafeSingleton() {
+    }
+
+    public static ThreadSafeSingleton getInstance() {
+        ThreadSafeSingleton result = instatce;
+        if (result == null) {
+            synchronized (mutex) {
+                result = instatce;
+                if (result == null) {
+                    result = instatce = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return result;
+    }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,29 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() throws SQLException {
+        Properties properties = getConnectionProperties();
+        return DriverManager.getConnection(
+                properties.getProperty("db_url"),
+                properties.getProperty("user"),
+                properties.getProperty("password")
+        );
+    }
+
+    @SneakyThrows
+    private Properties getConnectionProperties() {
+        Properties properties = new Properties();
+        properties.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
+        return properties;
+    }
 }
 
